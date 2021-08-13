058fa4c217382c973bef4773bf4defed498e1ca6
Bruna Alves <brunahalves94@gmail.com>
2021-08-13 23:51:49 +0200

    Add update item category

--- src/main/java/com/nfceanalysis/api/service/ItemService.java
+++ src/main/java/com/nfceanalysis/api/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.nfceanalysis.api.service;
 
 import com.nfceanalysis.api.model.Item;
+import com.nfceanalysis.api.repository.CategoryRepository;
 import com.nfceanalysis.api.repository.ItemRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class ItemService {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     public Item getItemById(String id){
         return itemRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Item Not Found with id: " + id));
@@ -22,16 +26,31 @@ public class ItemService {
 
     public List<Item> getItemByNfce(String nfce){
         return itemRepository.findByNfce(new ObjectId(nfce));
-               // .orElseThrow(() -> new NoSuchElementException("Item Not Found by nfce id: " + nfce));
     }
 
-    public Item updateCategorie(Item item){
+    public Item updateCategory(Item item){
         Item itemObj = itemRepository.findById(item.get_id())
-         .orElseThrow(() -> new NoSuchElementException("Item Not Found id: " + item.get_id()));
+                .orElseThrow(() -> new NoSuchElementException("Item Not Found id: " + item.get_id()));
 
-        itemObj.setCategorie(item.getCategorie());
+        itemObj.setCategory(item.getCategory());
         itemRepository.save(itemObj);
 
         return itemObj;
+    }
+
+    public Item updateByItemCode(Item item){
+        categoryRepository.findById(new ObjectId(item.getCategory().getId()))
+                .orElseThrow(() ->
+                        new NoSuchElementException("Category Not Found with id: " + item.getCategory().getId()));
+
+        List<Item> items = itemRepository.findByItemCode(item.getItemCode())
+                .orElseThrow(() -> new NoSuchElementException("Item Not Found id: " + item.getItemCode()));
+
+        for (Item it : items) {
+            it.setCategory(item.getCategory());
+            updateCategory(it);
+        }
+
+        return item;
     }
 }
